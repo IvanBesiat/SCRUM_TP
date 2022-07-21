4560655cd8dc6a10c2f15f31f8231d034ec3e836
IvanBesiat <ivan.besiat@gmail.com>
2022-07-21 12:49:18 +0200

    ajout methode gestion du stock lors de l'achat

--- Test_Scrum_TP/UnitTest1.cs
+++ Test_Scrum_TP/UnitTest1.cs
@@ -8,40 +8,56 @@ namespace Test_Scrum_TP
         [TestMethod]
         public void TestAchatCreation_CalculMontantCMD_Alcool()
         {
-            Achat_Manager createAchat = new Achat_Manager();
             Boisson boissonTest = new Boisson("Bière 33cl", 1.5,4.5,20,0);
             Dictionary<Boisson, int> boissonList = new Dictionary<Boisson, int>();
             boissonList.Add(boissonTest,1);
+            Achat_Manager createAchat = new Achat_Manager(boissonList);
 
-            double montantCMD = createAchat.CalculMontantCMD(boissonList);
+            double montantCMD = createAchat.CalculMontantCMD();
 
             Assert.AreEqual(montantCMD, 1.5);
         }
         [TestMethod]
         public void TestAchatCreation_CalculMontantCMD_Soft()
         {
-            Achat_Manager createAchat = new Achat_Manager();
-            Boisson boissonTest = new Boisson("Coca 33cl", 0.6, 4.5, 20, 0);
+            Boisson boissonTest = new Boisson("Coca 33cl", 0.6, 4.5, 10, 0);
             Dictionary<Boisson,int> boissonList = new Dictionary<Boisson, int>();
             boissonList.Add(boissonTest,1);
-
-            double montantCMD = createAchat.CalculMontantCMD(boissonList);
+            Achat_Manager createAchat = new Achat_Manager(boissonList);
+            
+            double montantCMD = createAchat.CalculMontantCMD();
 
             Assert.AreEqual(montantCMD, 0.6);
         }
         [TestMethod]
         public void TestAchatCreation_CalculMontantCMD_SoftPlusAlcool()
         {
-            Achat_Manager createAchat = new Achat_Manager();
             Boisson alcoolTest = new Boisson("Bière 33cl", 1.5, 4.5, 20, 0);
-            Boisson softTest = new Boisson("Coca 33cl", 0.6, 4.5, 20, 0);
+            Boisson softTest = new Boisson("Coca 33cl", 0.6, 4.5, 10, 0);
             Dictionary<Boisson,int> boissonList = new Dictionary<Boisson, int>();
             boissonList.Add(alcoolTest,1);
             boissonList.Add(softTest,2);
-            
-            double montantCMD = createAchat.CalculMontantCMD(boissonList);
+            Achat_Manager createAchat = new Achat_Manager(boissonList);
+
+            double montantCMD = createAchat.CalculMontantCMD();
 
             Assert.AreEqual(montantCMD, 2.7);
+        }
+
+        [TestMethod]
+        public void TestAchatCreation_AguementerStock_SoftPlusAlcool()
+        {
+            Boisson alcoolTest = new Boisson("Bière 33cl", 1.5, 4.5, 20, 0);
+            Boisson softTest = new Boisson("Coca 33cl", 0.6, 4.5, 10, 0);
+            Dictionary<Boisson, int> boissonList = new Dictionary<Boisson, int>();
+            boissonList.Add(alcoolTest, 1);
+            boissonList.Add(softTest, 2);
+            Achat_Manager createAchat = new Achat_Manager(boissonList);
+
+            string resultValue = createAchat.AuguementerStockBoisson();
+            string resultAttendu = string.Format("Le nouveau stock de {0} est de {1} unité(s).Le nouveau stock de {2} est de {3} unité(s).","Bière 33cl", 1, "Coca 33cl", 2);
+
+            Assert.AreEqual(resultAttendu, resultValue);
         }
     }
 }
