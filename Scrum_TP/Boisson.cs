5cd843c6c25cd5a43e3734ecf59ad0d9190c87e9
Dekleor <67908404+Dekleor@users.noreply.github.com>
2022-07-21 11:20:10 +0200

    ajout classe boisson

---
+++ Scrum_TP/Boisson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrum_TP
+{
+    internal class Boisson
+    {
+        public string Nom { get; set; }
+        public double PuAchat { get; set; }
+        public double PuVente { get; set; }
+        public int TVA { get; set; }
+        public int QuantiteStock { get; set; }
+    }
+}
