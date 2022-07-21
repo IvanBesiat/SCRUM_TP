d3c328f12f3f52347d3295e49f9b69f8ade3f6cf
IvanBesiat <ivan.besiat@gmail.com>
2022-07-21 11:56:25 +0200

    Mise en place de la methode de creation d'une commande

---
+++ Scrum_TP/Achat_Manager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrum_TP
+{
+    public class Achat_Manager
+    {
+        public Achat_Manager()
+        {
+        }
+
+        public double CalculMontantCMD(Dictionary<Boisson,int> boissons)
+        {
+            double prixCMD = 0;
+            foreach(KeyValuePair<Boisson,int> entry in boissons)
+            {
+                prixCMD += entry.Key.PuAchat * entry.Value;
+            }
+            return prixCMD;
+        }
+    }
+}
