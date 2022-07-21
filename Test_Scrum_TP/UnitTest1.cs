4fbc65da926ccfcd59af4cd2b7fe458e05388090
IvanBesiat <ivan.besiat@gmail.com>
2022-07-21 09:51:41 +0200

    mise a jour avec test

--- Test_Scrum_TP/UnitTest1.cs
+++ Test_Scrum_TP/UnitTest1.cs
@@ -4,8 +4,40 @@ namespace Test_Scrum_TP
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestIsFizz()
         {
+            int value = 3;
+
+            FizzBuzz testFB = new FizzBuzz(value);
+
+            Assert.AreEqual(testFB.NbrTest(value), "FIZZ");
+        }
+        [TestMethod]
+        public void TestIsBUzz()
+        {
+            int value = 5;
+
+            FizzBuzz testFB = new FizzBuzz(value);
+
+            Assert.AreEqual(testFB.NbrTest(value), "BUZZ");
+        }
+        [TestMethod]
+        public void TestIsFizzBuzz()
+        {
+            int value = 15;
+
+            FizzBuzz testFB = new FizzBuzz(value);
+
+            Assert.AreEqual(testFB.NbrTest(value), "FIZZBUZZ");
+        }
+        [TestMethod]
+        public void TestNoFizzNoBuzz()
+        {
+            int value = 2;
+
+            FizzBuzz testFB = new FizzBuzz(value);
+
+            Assert.AreEqual(testFB.NbrTest(value), "PERDU");
         }
     }
 }
