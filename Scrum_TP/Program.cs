a6c253135047e32a5df01188a9e8650aa1a5f64c
Dekleor <67908404+Dekleor@users.noreply.github.com>
2022-07-22 10:27:12 +0200

    ajout export xlsx

--- Scrum_TP/Program.cs
+++ Scrum_TP/Program.cs
@@ -7,8 +7,8 @@ namespace Scrum_TP
     {
         static void Main(string[] args)
         {
+            // import CSV
             List<Boisson> stockActuel = new List<Boisson>();
-
             using (TextFieldParser textFieldParser = new TextFieldParser(@"liste_boissons.csv"))
             {
                 textFieldParser.TextFieldType = FieldType.Delimited;
@@ -28,6 +28,32 @@ namespace Scrum_TP
                 stockActuel.Add(boisson);
                 }
             }
+
+            // export XSLX
+            var workbook = new XLWorkbook();
+            workbook.AddWorksheet("sheetName");
+            var ws = workbook.Worksheet("sheetName");
+
+            int row = 1;
+            foreach (object item in stockActuel)
+            {
+                if (row == 1)
+                {
+                    ws.Cell("A" + row.ToString()).Value = "Nom";
+                    ws.Cell("B" + row.ToString()).Value = "Prix unitaire d'achat";
+                    ws.Cell("C" + row.ToString()).Value = "Prix unitaire de vente";
+                    ws.Cell("D" + row.ToString()).Value = "TVA";
+                    ws.Cell("E" + row.ToString()).Value = "Quantité en Stock";
+                    row++;
+                }
+                ws.Cell("A" + row.ToString()).Value = "Nom";
+                ws.Cell("B" + row.ToString()).Value = "Prix unitaire d'achat";
+                ws.Cell("C" + row.ToString()).Value = "Prix unitaire de vente";
+                ws.Cell("D" + row.ToString()).Value = "TVA";
+                ws.Cell("E" + row.ToString()).Value = "Quantité en Stock";
+                row++;
+            }
+            workbook.SaveAs("yourExcel.xlsx");
         }
     }
 }
